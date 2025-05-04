8c27ec35d7eea05982c1a5204fd2af21bddf65e3
Anton Petrov <anton.petrov.main@gmail.com>
2025-05-04 15:46:02 +0300

    Add devpod config file.

--- setup.sh
+++ setup.sh
@@ -2,8 +2,14 @@
 rm $HOME/.tmux.conf
 rm $HOME/.bashrc
 rm $HOME/.gitconfig
+rm -rf $HOME/.devpod/config.yaml
+
 
 ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/bash/bashrc $HOME/.bashrc
 ln -s $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
+
+mkdir $HOME/.devpod
+ln -s $HOME/dotfiles/devpod/config.yaml $HOME/.devpod/config.yaml
+
 echo "Dotfiles installed!"
