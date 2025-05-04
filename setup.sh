714d3dcfe0e1ca0a8b37782ba45329eb895138fc
Anton Petrov <anton.petrov.main@gmail.com>
2025-05-04 12:41:32 +0000

    Delete old dotfiles every time new ones are installed. Use dotfiles instead of .dotfiles

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
-ln -s $HOME/.dotfiles/bash/bashrc $HOME/.bashrc
-ln -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
+rm $HOME/.tmux.conf
+rm $HOME/.bashrc
+rm $HOME/.gitconfig
+
+ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/bash/bashrc $HOME/.bashrc
+ln -s $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
+echo "Dotfiles installed!"
