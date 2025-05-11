dfd5d11ccce5b5bec7671e01e74b534087b71f56
Anton Petrov <anton.petrov.main@gmail.com>
2025-05-11 13:39:27 +0000

    update

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -36,6 +36,16 @@ return {
             })
           end,
         },
+        extensions = {
+        mcphub = {
+            callback = "mcphub.extensions.codecompanion",
+            opts = {
+                show_result_in_chat = true, -- Show the mcp tool result in the chat buffer
+                make_vars = true, -- make chat #variables from MCP server resources
+                make_slash_commands = true, -- make /slash_commands from MCP server prompts
+            },
+        }
+    },
       })
     end,
   },
@@ -183,7 +193,6 @@ return {
     },
   },
   {
-     enabled = false,
     "ravitemer/mcphub.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",  -- Required for Job and HTTP requests
