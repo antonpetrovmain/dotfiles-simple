977eaf8da1babd249bcdebbb45846de64d1eaa7d
Anton Petrov <anton.petrov.main@gmail.com>
2025-05-22 09:36:06 +0000

    update

--- setup.sh
+++ setup.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
-rm $HOME/.env
 rm $HOME/.tmux.conf
 rm $HOME/.bashrc
-rm $HOME/.gitconfig
-rm -rf $HOME/.devpod/config.yaml
 rm -rf $HOME/.config/nvim
-rm -rf $HOME/.config/nixpkgs
 
-
-ln -s $HOME/dotfiles/.env $HOME/.env
 ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/bash/bashrc $HOME/.bashrc
-ln -s $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
 
+# Neovim
 mkdir $HOME/.config
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
-
-mkdir $HOME/.devpod
-ln -s $HOME/dotfiles/devpod/config.yaml $HOME/.devpod/config.yaml
-
-mkdir $HOME/.config/nixpkgs
-ln -s $HOME/dotfiles/nix/config.nix $HOME/.config/nixpkgs/config.nix
-
-nix-env -iA nixpkgs.myPackages
 
 mkdir $HOME/.local/bin/
 sudo ln -s $HOME/dotfiles/lsp/jdtls $HOME/.local/bin/jdtls
 sudo ln -s $HOME/dotfiles/lsp/lua-language-server $HOME/.local/bin/lua-language-server
-export JAVA_HOME=/nix/store/`ls -l /nix/store | grep ^d.*openjdk-23 | awk '{print $9}'`
 
+echo "Dotfiles installed!"
+
+# My LLM CLI tool
 mkdir -p $HOME/tools/llm-cli
 cd $HOME/tools/llm-cli
 git clone git@github.com:antonpetrovmain/llm-cli.git
@@ -36,10 +24,8 @@ source .venv/bin/activate
 pip install -r requirements.txt
 deactivate
 cd -
-
 mkdir -p $HOME/.local/bin
 ln -s $HOME/dotfiles/tools/llm $HOME/.local/bin/llm
 ln -s $HOME/dotfiles/tools/llm-short $HOME/.local/bin/l
 
-
-echo "Dotfiles installed!"
+echo "My LLM CLI installed"
