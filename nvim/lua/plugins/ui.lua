cefe37e4b7bd61f657764d271b6d0d478c2453e5
Anton Petrov <anton.petrov.main@gmail.com>
2025-06-09 06:11:18 +0000

    update

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -20,14 +20,66 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
- config = function()
-    require("nvim-treesitter.configs").setup {
-      highlight = { enable = true },
-      indent    = { enable = true },
-      fold      = { enable = true, disable = {} },
-      ensure_installed = { "markdown", "markdown_inline", "lua", "python", "java", "json", "yaml", },
-      auto_install     = true,}
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        highlight        = { enable = true },
+        indent           = { enable = true },
+        fold             = { enable = true, disable = {} },
+        ensure_installed = { "markdown", "markdown_inline", "lua", "python", "java", "json", "yaml", },
+        auto_install     = true, }
     end
 
-    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- Load the custom component
+      local codecompanion_spinner = require("lualine.components.codecompanion")
+
+      require("lualine").setup({
+        options = {
+          theme = "auto", -- or your preferred theme
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+          disabled_filetypes = { statusline = {}, winbar = {} },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+            refresh_time = 16, -- ~60fps
+          },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = {
+            codecompanion_spinner,
+            "encoding",
+            "fileformat",
+            "filetype",
+          },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
+      })
+    end,
+  },
+
 }
