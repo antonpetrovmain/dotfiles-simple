ac21a4a5292e81b0a5c76f239897973e51d9da1a
Anton Petrov <anton.petrov.main@gmail.com>
2025-06-17 18:24:55 +0000

    update

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,7 +1,8 @@
 return { 
 	{ "mason-org/mason.nvim", cmd = "Mason", opts = {} },
   { "WhoIsSethDaniel/mason-tool-installer.nvim", opts = {
-    ensure_installed = {  'lua-language-server',   'basedpyright', 'jdtls'},
+    ensure_installed = {  'lua-language-server',   'basedpyright', 'jdtls', 
+      'black', 'pylint', 'flake8',},
 
   }},
 
